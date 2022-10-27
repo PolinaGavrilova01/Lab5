b5081609a74952097a3449ec3ba7c1b2292197e8
PolinaGavrilova01 <pgavri200@caledonian.ac.uk>
2022-10-27 11:40:20 +0000

    added code

---
+++ HelloWorld.java
@@ -0,0 +1,36 @@
+/* This is a simple Java program. 
+   FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+}
+  GNU nano 2.9.3                         HelloWorld.java
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                      [ Read 0 lines ]
+^G Get Help    ^O Write Out   ^W Where Is    ^K Cut Text    ^J Justify     ^C Cur Pos
+^X Exit        ^R Read File   ^\ Replace     ^U Uncut Text  ^T To Spell    ^_ Go To Line
+
